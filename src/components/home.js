02740b968d3c0c563f8c6758290862d951554a0e
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-06-21 18:02:21 -0400

    Finished home tab

--- src/components/home.js
+++ src/components/home.js
@@ -12,6 +12,23 @@ const loadHomeTab = () => {
         sectionHeader.classList.add("section-header");
         sectionHeader.textContent = title;
         container.append(sectionHeader);
+
+        const sectionContent = document.createElement("div");
+        sectionContent.classList.add("section-content");
+        sectionContent.textContent = content;
+        container.append(sectionContent);
     }
 
-}
+    createSection(
+        "Welcome to Dust Bowl:",
+        "At Dust Bowl, we bring you the best of Southern Western BBQ. Join us for a true taste of the Southwest and genuine Southern hospitality."
+    );
+
+    createSection(
+        "What We Do:",
+        "Nestled in the heart of our community, Dust Bowl is your go-to destination for authentic Southern Western BBQ. Our passion for flavor and tradition is evident in every dish we serve, from our slow-smoked brisket to our tangy, house-made sauces. Whether you’re here for a hearty meal, a family gathering, or just to experience true Southern hospitality, we promise an unforgettable dining experience that celebrates the rich culinary heritage of the Southwest. "
+    );
+
+};
+
+export default loadHomeTab;
