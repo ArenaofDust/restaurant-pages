70d8fdfebe5e5324b618b78f24880685702ad8a9
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-06-21 18:30:56 -0400

    Added css and style loaders

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/style.css"
+import loadHomeTab from "./components/home";
+
+loadHomeTab();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     plugins: [
         new HTMLWebpackPlugin({
@@ -13,5 +14,13 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
 };
