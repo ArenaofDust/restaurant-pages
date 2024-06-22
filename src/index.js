fd6574cc391dec9962eda8726c18545e50b2a63c
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-06-21 22:58:05 -0400

    Added gaps for menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "./styles/style.css"
 import loadHomeTab from "./components/home";
+import loadAboutTab from "./components/about";
+import loadMenuTab from "./components/menu";
+
+const buttonListener = () =>  {
+    document.getElementById("home").addEventListener("click", loadHomeTab);
+    document.getElementById("about").addEventListener("click", loadAboutTab);
+    document.getElementById("menu").addEventListener("click", loadMenuTab);
+
+};
+
 
 loadHomeTab();
+buttonListener();
